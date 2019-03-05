8934907d555d91bcf2311113b09ead15bba6cdb8
jsulli <josephs3d@gmail.com>
2019-03-04 19:00:02 -0600

    tests

--- app.js
+++ app.js
@@ -3,23 +3,30 @@ const whois = require('whois')
 
 exports.handler = (event, context, callback) => {
 
-    whois.lookup(event.address, (err, data) => {
-        if(err) {
-            callback(null, "Failed to get data on address")
-        }
-        callback(null, processData(data));
+    lookupAddress(event.address, response => {
+        callback(null, response)
     })
 }
 
+
+export const lookupAddress = (address, callback) => {
+    whois.lookup(address, (err, data) => {
+        if(err) {
+            callback("Failed to get data on address")
+        }
+        callback(processData(data));
+    })
+}
+
+
 // filter out extra junk related to the whois lookup itself and not the address
-const processData = (data) => {
+export const processData = (data) => {
 
     let arr = data.split("\n")
 
     let processed = ""
     for(let i = 0; i < arr.length; i++) {
         const str = arr[i];
-        console.log(str)
         if(str.indexOf("Registrar") > -1) continue
         if(str.indexOf("Domain Status") > -1) continue
         if(str.indexOf("DNSSEC") > -1) break;
@@ -27,8 +34,7 @@ const processData = (data) => {
             // an invalid address prints out a bunch of stuff related to the whois service, we don't need to see it, return an error
             // Lambda always returns status code 200 if the function is successfully reached,
             // so I can't seem to return a useful error code. A string body has to suffice
-            processed = "ERROR"
-            break;
+            return "ERROR_BAD_ADDRESS"
         }
 
         processed += str + "\n"
---
+++ test/process.test.js
@@ -0,0 +1,40 @@
+import {lookupAddress, processData} from "../app"
+
+
+describe('get address results ', () => {
+
+    it('finds good data on valid URL', () => {
+        lookupAddress('google.com', (response) => {
+            expect(response).toContain("Google LLC")
+        })
+    })
+
+    it('finds good data on valid IP address', () => {
+        lookupAddress('8.8.8.8', (response) => {
+            expect(response).toContain("American Registry for Internet Numbers")
+        })
+    })
+})
+
+describe('Process address result', () => {
+
+    it('Removes entries with Registrar', () => {
+        expect(processData("Registrar")).not.toContain("Registrar")
+    })
+
+    it('removes entries with Domain Status', () => {
+        expect(processData("Domain Status")).not.toContain("Domain Status")
+    })
+
+    it('removes entries with Domain Status', () => {
+        expect(processData("Domain Status")).not.toContain("Domain Status")
+    })
+
+    it('removes entries with DNSSEC', () => {
+        expect(processData("DNSSEC")).not.toContain("DNSSEC")
+    })
+
+    it('returns error code with an invalid address', () => {
+        expect(processData("% This is the RIPE Database query service.")).toBe("ERROR_BAD_ADDRESS")
+    })
+})
